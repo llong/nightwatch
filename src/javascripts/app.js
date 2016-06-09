6259d70eb1aa5dbef5c752042b968ef0b511919a
Lewis Long <llongdesigns@gmail.com>
2016-06-08 20:36:52 -0400

    added more views

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -15,11 +15,76 @@ app.config(['$routeProvider','$locationProvider',function($routeProvider,$locati
     .when('/add-phone', {
       templateUrl: '/assets/partials/add-phone.html'
     })
+    .when('/primary-address', {
+      templateUrl: '/assets/partials/primary-address.html'
+    })
+    .when('/billing-address', {
+      templateUrl: '/assets/partials/billing-address.html'
+    })
+    .when('/create-subaccount', {
+      templateUrl: '/assets/partials/create-subaccount.html'
+    })
+    .when('/create-group', {
+      templateUrl: '/assets/partials/create-group.html'
+    })
+    .when('/add-device', {
+      templateUrl: '/assets/partials/add-device.html'
+    })
+    .when('/add-horse', {
+      templateUrl: '/assets/partials/add-horse.html'
+    })
+    .when('/notification-settings', {
+      templateUrl: '/assets/partials/notification-settings.html'
+    })
     .otherwise({redirectTo:'/login'});
 
   //  $locationProvider.html5Mode(true);
 }]);
 
-app.controller('mainController', function(){
 
-})
+app.controller('userController',['$scope', function($scope){
+  $scope.showUserForm = function() {
+    $scope.skipButton = false;
+    $scope.AddUserForm = true;
+    console.log('click');
+  }
+}]);
+
+app.controller('groupController',['$scope', function($scope){
+  $scope.group = false;
+  $scope.groups = [];
+  $scope.assignGroupLink = '';
+
+  // Enables/Disables associated address form for group
+  $scope.assignGroup = function(){
+    if ($scope.group) {
+      $scope.group = false;
+      this.innerHTML = 'Don\'t Assign Address To Group';
+    } else {
+      $scope.group = true;
+      this.innerHTML = 'Don\'t Assign Address To Group';
+    }
+  }
+  // Adds a new group to user groups
+  $scope.addGroup = function(){
+    $scope.groups.push({
+      groupName: $scope.groupName,
+      address: $scope.address,
+      address2: $scope.address2,
+      city: $scope.city,
+      state: $scope.state,
+      country: $scope.country
+    });
+
+    // Clear form fields
+    $scope.groupName = '';
+    $scope.address = '';
+    $scope.address2 = '';
+    $scope.city = '';
+    $scope.state = '';
+    $scope.country = '';
+    // hide address fields
+    $scope.group = false;
+  }
+
+}]);
