43f65699f94271bd36a6f1d4933bb4f82d44a6e2
Lewis Long <llongdesigns@gmail.com>
2016-06-19 22:33:00 -0400

    removed test function

--- src/javascripts/controllers/main-controller.js
+++ src/javascripts/controllers/main-controller.js
@@ -4,16 +4,6 @@ function($scope,$http,$localStorage,$location,authenticate){
 
   var API = authenticate;
 
-  $scope.getData = function(){
-    $http.get(API + 'api/account',{
-
-    })
-    .then(function(res){
-      $scope.userData = res.data;
-      console.log($scope.userData);
-    });
-  };
-
   // Hamburger Menu
   $scope.toggleMenu = function(){
     $scope.expanded = !$scope.expanded;
